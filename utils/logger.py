a1c93cf92dfed2d43065732dd421e8eee08e03a4
Youssefzdb <149632182+Youssefzdb@users.noreply.github.com>
2026-04-22 23:37:14 +0100

    feat: add logger utility

--- utils/logger.py
+++ utils/logger.py
@@ -1,11 +1,20 @@
-import logging
-import sys
+#!/usr/bin/env python3
+"""Logger Utility"""
 
-def setup_logger(verbose=False):
-    logger = logging.getLogger("shadow-attack")
-    logger.setLevel(logging.DEBUG if verbose else logging.INFO)
-    handler = logging.StreamHandler(sys.stdout)
-    formatter = logging.Formatter("\033[90m%(asctime)s\033[0m %(message)s", datefmt="%H:%M:%S")
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
-    return logger
+class Logger:
+    RED = "\033[91m"
+    GREEN = "\033[92m"
+    YELLOW = "\033[93m"
+    BLUE = "\033[94m"
+    RESET = "\033[0m"
+
+    def __init__(self, verbose: bool = False):
+        self.verbose = verbose
+
+    def info(self, msg): print(f"{self.BLUE}[*]{self.RESET} {msg}")
+    def success(self, msg): print(f"{self.GREEN}[+]{self.RESET} {msg}")
+    def warning(self, msg): print(f"{self.YELLOW}[!]{self.RESET} {msg}")
+    def error(self, msg): print(f"{self.RED}[-]{self.RESET} {msg}")
+    def debug(self, msg):
+        if self.verbose:
+            print(f"[DEBUG] {msg}")
